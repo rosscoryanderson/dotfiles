f0de2af35e1203b0f57fdaf0dbba4c942f39110e
Ross Anderson <rossanderson@Rosss-MacBook-Pro-2.local>
2025-02-19 11:58:17 +0800

    tmux layout script

---
+++ tools/default_layout_init.sh
@@ -0,0 +1,11 @@
+tmux split-window -h
+tmux split-window -v
+tmux split-window -v
+tmux select-pane -t 0
+tmux split-window -v
+
+tmux resize-pane -Dt 1 25 
+tmux resize-pane -Rt 0 82 
+tmux resize-pane -Ut 2 21 
+tmux resize-pane -Dt 4 1 
+
