a2c78ac5e48e9da9722d7f62573d304ed30b289b
Ross Anderson <rossanderson@Rosss-MacBook-Pro-2.local>
2025-02-24 11:36:15 +0800

    removing old config

--- nvim/.config/nvim/lua/rosscoryanderson/plugins/lexima.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "cohama/lexima.vim",
-    -- config = function()
-    --     require("lexima").setup({})
-    -- end
-}
--- nvim/.config/nvim/lua/rosscoryanderson/plugins/undotree.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "mbbill/undotree",
-  lazy = false,
-  config = function()
-    vim.keymap.set("n", "<leader>uu", vim.cmd.UndotreeToggle)
-    -- require("undotree").setup({})
-  end
-}
